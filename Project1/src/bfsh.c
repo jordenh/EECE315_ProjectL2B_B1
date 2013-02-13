1cee79c372fb948604dc6b7783a0fa1340ecafb6
BlueMonday <stevenerenst@gmail.com>
2013-02-13 00:51:19 -0800

    cleaned up the code

--- Project1/src/bfsh.c
+++ Project1/src/bfsh.c
@@ -1,5 +1,6 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include<string.h>
 
 #define BUFFER 300
 #define TRUE 1
@@ -14,82 +15,73 @@ struct command_t {
 typedef struct command_t command;
 
 int main(void){
+	int exitBool = FALSE;
+	//char* pwd;
 	command nextCommand ;	
 	int commandCount = 0;
-	int exitBool = FALSE;
-	int i,j;
-	int charParserIndex = 0;
 	int individualCharIndex = 0;
-	char tempChar;
-	int nbytes= BUFFER*sizeof(char);
-	int bytes_read;
+	int nbytes= BUFFER;
+	char* tempChar=malloc(nbytes+1);
+	nextCommand.argc = 0;
+
 
 	nextCommand.name = (char*)malloc(nbytes+1);
-	for(i=0;i<100;i++){
+	for(int i=0;i<100;i++){
 		nextCommand.argv[i] = (char*)malloc(nbytes+1);
 	}    
 
 
-	printf("The value of argc is: %d\n" ,nextCommand.argc);
-	printf("The value of name is: %s\n" ,nextCommand.name);
-
-	nextCommand.argc = 0;
 	while(!exitBool){
 		//clear argv/argc/name for the next input.			
-        	for(i=0;i<=nextCommand.argc;i++){
-	            for(j=0;j<BUFFER;j++){
+        	for(int i=0;i<=nextCommand.argc;i++){
+	            for(int j=0;j<BUFFER;j++){
         	        nextCommand.argv[i][j] = '\0'; //replace all of allocated buffer size to null
         	    }
         	}
-		for(i=0;i<BUFFER;i++){
+		for(int i=0;i<BUFFER;i++){
 			nextCommand.name[i] = '\0';
 		}	
         	 
         	nextCommand.argc = 0;
-		charParserIndex = 0;
 		individualCharIndex = 0;		
 
 		printf("cmd%d:~myDirectoryValue> ", commandCount);
-		bytes_read = getline(&nextCommand.argv[0], &nbytes, stdin);
+		fgets(tempChar, (nbytes+1), stdin);
 		
-		tempChar = nextCommand.argv[0][charParserIndex];
-		while(tempChar != '\n' ){
+		while(*tempChar != '\n' ){
 			if(nextCommand.argc == 0){
-				if(tempChar != 0x20){
-					nextCommand.name[individualCharIndex++] = nextCommand.argv[0][charParserIndex];
+				if(*tempChar != 0x20){
+					nextCommand.name[individualCharIndex++] = *tempChar;
 				}
 				else{
 					nextCommand.argc++;  //move to next string
 					individualCharIndex = 0;
-					printf("ping1%s,%d,%d\n", nextCommand.name, nextCommand.argc,charParserIndex);
+					//printf("ping1%s,%d,%d\n", nextCommand.name, nextCommand.argc,charParserIndex);
 				}
 			}
 			else{
-				if(tempChar != 0x20){
-					nextCommand.argv[nextCommand.argc][individualCharIndex++] = nextCommand.argv[0][charParserIndex];
+				if(*tempChar != 0x20){
+					nextCommand.argv[nextCommand.argc][individualCharIndex++] = *tempChar;
 				}
 				else{
 					nextCommand.argc++;  //move to next string
 					individualCharIndex = 0;
 
-					printf("ping2%s,%d\n", nextCommand.argv[nextCommand.argc], nextCommand.argc);
+					//printf("ping2%s,%d\n", nextCommand.argv[nextCommand.argc], nextCommand.argc);
 				}
 			}
-			charParserIndex++;
-			tempChar = nextCommand.argv[0][charParserIndex];
+			tempChar++;
 		}
 
 		strcpy(nextCommand.argv[0],nextCommand.name);
 		
-		for(i=0; i<=nextCommand.argc; i++){
+		for(int i=0; i<=nextCommand.argc; i++){
 			printf("-%s-\n",nextCommand.argv[i]);
 		}
 
 		if(1){
 			commandCount++;
 		}
-
-        //PERFORM ACTION HERE
 
         
 	}
