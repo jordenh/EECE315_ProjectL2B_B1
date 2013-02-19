b14d3dea1d94a05b78a69ebb989f2ecadb4c0a51
BlueMonday <stevenerenst@gmail.com>
2013-02-19 02:20:08 -0800

    parser functionality

---
+++ Project1/src/parser.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
+#include "bfsh.h"
+#include "parser.h"
+
+void parse_arguments(command *nextCommand, char *tempString){
+	int i = 0, j = 0;
+	int previous_char_is_alphanum = 0;
+	int found_quotes = 0;
+	char buffer[80];		
+	
+	while(1){
+		if(*tempString == ' ' && previous_char_is_alphanum){
+			if(!found_quotes){
+				buffer[i] = '\0';
+				strcpy(nextCommand->argv[j++], buffer);
+				previous_char_is_alphanum = 0;
+				nextCommand->argc++;
+				i = 0;
+			}
+			else
+				buffer[i++] = *tempString;	
+		}
+		else if(*tempString == '\"' || *tempString == '\''){
+			found_quotes = ~(found_quotes);
+		}
+		else if(*tempString == '\0'){
+			if(previous_char_is_alphanum){
+				buffer[i] = '\0';
+				strcpy(nextCommand->argv[j], buffer);
+				nextCommand->argc++;
+			}
+
+			strcpy(nextCommand->name, nextCommand->argv[0]);
+			break;
+		}
+		else if(*tempString != ' '){
+			buffer[i++] = *tempString;
+			previous_char_is_alphanum = 1;
+		}	
+
+		tempString++;		
+	}
+}
---
+++ Project1/src/parser.h
@@ -0,0 +1,4 @@
+#ifndef PARSER_H
+#define PARSER_H
+	void parse_arguments(command *nextCommand, char *tempString);
+#endif 
