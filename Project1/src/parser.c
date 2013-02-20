9c6c5bcfea7309f0bb9558550976d930a035ac29
BlueMonday <stevenerenst@gmail.com>
2013-02-20 03:17:42 -0800

    implement dollar sign char replacement for enviromental variables

--- Project1/src/parser.c
+++ Project1/src/parser.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<ctype.h>
 #include<string.h>
 #include "bfsh.h"
 #include "parser.h"
@@ -8,17 +7,35 @@
 void parse_arguments(command *nextCommand, char *tempString){
 	int i = 0, j = 0;
 	int previous_char_is_alphanum = 0;
+	int found_dollar_char = 0;
 	int found_quotes = 0;
 	char buffer[80];		
+	char *shifted_buffer;
 	
 	while(1){
 		if(*tempString == ' ' && previous_char_is_alphanum){
 			if(!found_quotes){
 				buffer[i] = '\0';
-				strcpy(nextCommand->argv[j++], buffer);
-				previous_char_is_alphanum = 0;
-				nextCommand->argc++;
-				i = 0;
+				if(found_dollar_char){
+					found_dollar_char = 0;
+					shifted_buffer = malloc(strlen(buffer));
+
+					for(int k = 0; k<strlen(buffer); k++)
+						*(shifted_buffer + k) = buffer[k+1];
+				
+					shifted_buffer = getenv(shifted_buffer);
+					strcpy(nextCommand->argv[j++], shifted_buffer);
+					free(shifted_buffer);
+					previous_char_is_alphanum = 0;
+					nextCommand->argc++;
+					i = 0;
+				}
+				else{
+					strcpy(nextCommand->argv[j++], buffer);
+					previous_char_is_alphanum = 0;
+					nextCommand->argc++;
+					i = 0;
+				}
 			}
 			else
 				buffer[i++] = *tempString;	
@@ -33,7 +50,18 @@ void parse_arguments(command *nextCommand, char *tempString){
 			}
 			else if(previous_char_is_alphanum){
 				buffer[i] = '\0';
-				strcpy(nextCommand->argv[j], buffer);
+				if(found_dollar_char){
+					found_dollar_char = 0;
+					shifted_buffer = malloc(strlen(buffer));
+
+					for(int k = 0; k<strlen(buffer); k++)
+						*(shifted_buffer + k) = buffer[k+1];
+				
+					shifted_buffer = getenv(shifted_buffer);
+					strcpy(nextCommand->argv[j], shifted_buffer);
+				}
+				else
+					strcpy(nextCommand->argv[j], buffer);
 			}
 			else if(j == 0){
 				strcpy(nextCommand->argv[0],"\0");
@@ -42,7 +70,11 @@ void parse_arguments(command *nextCommand, char *tempString){
 			strcpy(nextCommand->name, nextCommand->argv[0]);
 			break;
 		}
+
 		else if(*tempString != ' '){
+			if(*tempString == '$')
+				found_dollar_char = 1;
+					
 			buffer[i++] = *tempString;
 			previous_char_is_alphanum = 1;
 		}	
